e7cc9a74f62266a1fc3948d7f7ac9d51965163a6
Douglas <douglas.ferreira@varsis.com.br>
2020-07-27 09:10:55 -0300

    Atualizacoes

--- CriarMap/CriarMap/Program.cs
+++ CriarMap/CriarMap/Program.cs
@@ -15,6 +15,8 @@ namespace CriarMap
             StreamReader y;
             StreamWriter z;
             StreamWriter k;
+            StreamWriter jj;
+            StreamWriter rr;
 
             string t;
             string campo = null;
@@ -22,17 +24,50 @@ namespace CriarMap
             string campou;
             string lin;
             string linMdl = null;
+            string linJson = null;
+            string linr = null;
             string tipo;
 
-            string a = "c:\\git\\PROJETOS\\DGS\\cs\\CriarMap\\vspagtitulos3Srv.txt";
-            string b = "c:\\git\\PROJETOS\\DGS\\cs\\CriarMap\\map-vsrettitulos3.txt";
-            string c = "c:\\git\\PROJETOS\\DGS\\cs\\CriarMap\\map-tipo-vsrectitulos3.txt";
-            string d = "c:\\git\\PROJETOS\\DGS\\cs\\CriarMap\\mdl-vsrectitulos3.txt";
+            string a = "c:\\git\\PROJETOS\\DGS\\cs\\CriarMap\\tabela.txt";
+            string b = "c:\\git\\PROJETOS\\DGS\\cs\\CriarMap\\map-u-tabela.txt";
+            string c = "c:\\git\\PROJETOS\\DGS\\cs\\CriarMap\\tipos-tabela.txt";
+            string d = "c:\\git\\PROJETOS\\DGS\\cs\\CriarMap\\mdl-tabela.txt";
+            string j = "c:\\git\\PROJETOS\\DGS\\cs\\CriarMap\\json-tabela.txt";
+            string r = "c:\\git\\PROJETOS\\DGS\\cs\\CriarMap\\to-record-tabela.txt";
 
             y = File.OpenText(a);
             x = File.CreateText(b);
             z = File.CreateText(c);
             k = File.CreateText(d);
+            jj = File.CreateText(j);
+            rr = File.CreateText(r);
+
+            linMdl = "public override string EntityName => xx;";
+            k.WriteLine(linMdl);
+            linMdl = "public string Name " + "{" + " get; set; " + "}";
+            k.WriteLine(linMdl);
+            linMdl = "public string Code " + "{" + " get; set; " + "}";
+            k.WriteLine(linMdl);
+
+            lin = "map.Add(\"" + "name" + "\",\"" + "T" + "\");";
+            z.WriteLine(lin);
+            lin = "map.Add(\"" + "code" + "\",\"" + "T" + "\");";
+            z.WriteLine(lin);
+
+            lin = "map.Add(\"" + "name" + "\",\"" + "Name" + "\");";
+            x.WriteLine(lin);
+            lin = "map.Add(\"" + "code" + "\",\"" + "Code" + "\");";
+            x.WriteLine(lin);
+
+            linJson = "record.Name = entity.RecId.ToString();";
+            jj.WriteLine(linJson);
+            linJson = "record.Code = entity.RecId.ToString();";
+            jj.WriteLine(linJson);
+
+            linr = $"entity.Name = record.Name;";
+            rr.WriteLine(linr);
+            linr = $"entity.Code = record.Code;";
+            rr.WriteLine(linr);
 
             while (!y.EndOfStream)
             {
@@ -43,9 +78,15 @@ namespace CriarMap
                     campo = campo.Replace("\"", null);
                     campo = campo.Replace(",", null);
                     campou = "U_" + campo;
-                    lin = "map.Add(\"" + campo + "\",\"" +  campou.ToUpper() + "\");";                    
+                    lin = "map.Add(\"" + campo.ToLower() + "\",\"" +  campou.ToUpper() + "\");";                    
                     Console.WriteLine(lin);
                     x.WriteLine(lin);
+
+                    linJson = $"record.{campou.ToUpper()} = entity.{campo.Substring(0, 1).ToUpper()}{campo.Substring(1).ToLower()};";
+                    jj.WriteLine(linJson);
+
+                    linr = $"entity.{campo.Substring(0, 1).ToUpper()}{campo.Substring(1).ToLower()} = record.{campou.ToUpper()};";
+                    rr.WriteLine(linr);
 
                 }
                 if (t.Trim().StartsWith("dataType ="))
@@ -58,29 +99,29 @@ namespace CriarMap
                     if (campoT == "db_Numeric")
                     {
                         tipo = "N";
-                        linMdl = "public long " + campo + " { get; set; " + "}";
+                        linMdl = "public long " + campo.Substring(0, 1) + campo.Substring(1).ToLowerInvariant() + " { get; set; " + "}";
                     }
                         
                     if (campoT == "db_Alpha")
                     {                        
-                        linMdl ="public string " + campo + " { get; set; " + "}";
+                        linMdl ="public string " + campo.Substring(0, 1) + campo.Substring(1).ToLowerInvariant() + " { get; set; " + "}";
                         tipo = "T"; 
                     }
                        
                     if (campoT == "db_Float")
                     {
-                        linMdl = "public double " + campo + " { get; set; " + "}";
+                        linMdl = "public double " + campo.Substring(0,1) + campo.Substring(1).ToLowerInvariant() + " { get; set; " + "}";
                         tipo = "N";
                     }
                        
                     if (campoT == "db_Date")
                     {
-                        linMdl = "public DateTime " + campo + " { get; set; " + "}";
+                        linMdl = "public DateTime " + campo.Substring(0, 1) + campo.Substring(1).ToLowerInvariant() + " { get; set; " + "}";
                         tipo = "T";
                     }                        
                     
                     campou = tipo;
-                    lin = "map.Add(\"" + campo + "\",\"" + campou.ToUpper() + "\");";
+                    lin = "map.Add(\"" + campo.ToLower() + "\",\"" + campou.ToUpper() + "\");";
                                        
                     Console.WriteLine(lin);
 
@@ -93,6 +134,8 @@ namespace CriarMap
             x.Close();
             z.Close();
             k.Close();
+            jj.Close();
+            rr.Close();
             Console.WriteLine("Pressione uma tecla para finalizar");
             Console.ReadLine();
                 
