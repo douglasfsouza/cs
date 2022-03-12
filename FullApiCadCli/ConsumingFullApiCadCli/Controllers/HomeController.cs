93a6dd734ea3f19f6080cde9d48df0db34e8a14b
Douglas <douglas.ferreira@varsis.com.br>
2022-03-12 18:51:42 -0300

    Api simples criando e consumindo em aspnet e js

---
+++ FullApiCadCli/ConsumingFullApiCadCli/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using ConsumingFullApiCadCli.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ConsumingFullApiCadCli.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public HomeController(IConfiguration config)
+        {
+            _config = config;
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            string url = _config["urlApi"];
+            var req = new HttpClient();
+            var resp = await req.GetAsync(url);
+            Cliente cli = null;
+            if (resp.IsSuccessStatusCode)
+            {
+                var responseBody = await resp.Content.ReadAsStringAsync();
+                cli = JsonConvert.DeserializeObject<Cliente>(responseBody);
+            }
+            
+            return View(cli);
+        }
+    }
+}
---
+++ FullApiCadCli/FullApiCadCli/Controllers/Index.cs
@@ -0,0 +1,29 @@
+﻿using FullApiCadCli.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullApiCadCli.Controllers
+{
+    public class Index : Controller
+    {
+        [HttpGet("getCliente")]
+        public IActionResult GetCliente()
+        {
+            Cliente cli = 
+             new Cliente
+            {
+                Id = 1,
+                Nome = "Douglas Ferreira"
+            };
+            return Ok(cli);
+
+            //return BadRequest("Cliente não encontrado");
+
+        }
+        
+    }
+}
