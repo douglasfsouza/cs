cde556e7661338c9acf3b667fbcde776d7dea714
Douglas <douglas.ferreira@varsis.com.br>
2022-05-17 22:31:15 -0300

    Programa para dividir um arquivo grande em N arquivos

---
+++ BreakFiles/BreakFiles/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace ReplaceCV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPasta = new System.Windows.Forms.TextBox();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.grdFiles = new System.Windows.Forms.DataGridView();
+            this.txtLinhasPorArquivo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.grdFiles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Folder:";
+            // 
+            // txtPasta
+            // 
+            this.txtPasta.Location = new System.Drawing.Point(43, 0);
+            this.txtPasta.Name = "txtPasta";
+            this.txtPasta.Size = new System.Drawing.Size(823, 20);
+            this.txtPasta.TabIndex = 1;
+            this.txtPasta.Text = "e:\\cnab";
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(872, 198);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(75, 27);
+            this.btnReplace.TabIndex = 2;
+            this.btnReplace.Text = "Quebrar";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Image = ((System.Drawing.Image)(resources.GetObject("btnBrowse.Image")));
+            this.btnBrowse.Location = new System.Drawing.Point(872, 0);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 53);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // grdFiles
+            // 
+            this.grdFiles.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.grdFiles.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.grdFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdFiles.ColumnHeadersVisible = false;
+            this.grdFiles.Location = new System.Drawing.Point(43, 27);
+            this.grdFiles.Name = "grdFiles";
+            this.grdFiles.RowHeadersVisible = false;
+            this.grdFiles.Size = new System.Drawing.Size(823, 198);
+            this.grdFiles.TabIndex = 4;
+            // 
+            // txtLinhasPorArquivo
+            // 
+            this.txtLinhasPorArquivo.Location = new System.Drawing.Point(872, 172);
+            this.txtLinhasPorArquivo.Name = "txtLinhasPorArquivo";
+            this.txtLinhasPorArquivo.Size = new System.Drawing.Size(75, 20);
+            this.txtLinhasPorArquivo.TabIndex = 5;
+            this.txtLinhasPorArquivo.Text = "500";
+            this.txtLinhasPorArquivo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(872, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Qtde Linhas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(959, 237);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLinhasPorArquivo);
+            this.Controls.Add(this.grdFiles);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnReplace);
+            this.Controls.Add(this.txtPasta);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Quebrar arquivos de Extrato bancario";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdFiles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPasta;
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.DataGridView grdFiles;
+        private System.Windows.Forms.TextBox txtLinhasPorArquivo;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ BreakFiles/BreakFiles/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReplaceCV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            
+            txtPasta.Text = folderBrowserDialog1.SelectedPath;
+
+            if (!Directory.Exists(txtPasta.Text))
+            {
+                MessageBox.Show("Pasta inexistente");
+                return;
+            }
+
+            loadFiles();      
+
+
+        }
+        void loadFiles()
+        {
+            string[] files = Directory.GetFiles(txtPasta.Text);
+
+            List<Files> lstFiles = new List<Files>();
+
+
+//            listView1.Clear();
+            foreach (var file in files)
+            {
+
+                //listView1.Items.Add(file);
+
+                if (!file.EndsWith(".exe"))
+                {
+                    DataGridViewColumn c = new DataGridViewColumn();
+
+                    lstFiles.Add(new Files() { File = file });
+                }
+
+                
+
+                
+                
+            }
+            grdFiles.DataSource = lstFiles;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = AppContext.BaseDirectory;
+            txtPasta.Text = folderBrowserDialog1.SelectedPath;
+            loadFiles();
+        }
+
+        private bool Break()
+        {
+            StreamReader r;
+            StreamWriter w;     
+
+            string[] files = Directory.GetFiles(txtPasta.Text);
+            int errors = 0;
+            int countFiles = 1;
+            long linesPerFile = Convert.ToInt64(txtLinhasPorArquivo.Text);
+            if (linesPerFile == 0) linesPerFile = 500;
+
+            foreach (var file in files)
+            {
+                if (!file.EndsWith(".ret"))
+                {
+                    string newFileName = $"{file.Substring(0,file.Length -4)}-{countFiles}.ret";
+                    string cab1 = string.Empty;
+                    string cab2 = string.Empty;
+                    string cab3 = string.Empty;
+
+                    r = File.OpenText(file);
+                    w = File.CreateText(newFileName);                    
+
+                    while (!r.EndOfStream)
+                    {
+                        //inicia arquivo novo
+                        long countNew = 0;
+                        while (countNew++ < linesPerFile)
+                        {
+                            if (r.EndOfStream)
+                                break;
+
+                            string line = r.ReadLine();
+                         
+                            w.WriteLine(line);
+
+                            if (cab1 == string.Empty)
+                            {
+                                cab1 = line;
+                            }
+                            if (countNew == 2 && cab2 == string.Empty)
+                            {
+                                cab2 = line;
+                            }
+                            if (countNew == 3 && cab3 == string.Empty)
+                            {
+                                cab3 = line;
+                            }
+                        }
+                        w.Close();
+                        if (!r.EndOfStream)
+                        {
+                            newFileName = $"{file.Substring(0, file.Length - 4)}-{++countFiles}.ret";
+                            w = File.CreateText(newFileName);
+                            w.WriteLine(cab1);
+                            w.WriteLine(cab2);
+                            w.WriteLine(cab3);
+                        }
+                    }
+                    r.Close();
+                }
+            }
+            return true;
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            if (Break())
+            {
+                MessageBox.Show("Arquivos gerados com sucesso");
+            }
+        }
+
+        private class Files
+        {
+            public string File { get; set; }
+        }
+    }
+}
