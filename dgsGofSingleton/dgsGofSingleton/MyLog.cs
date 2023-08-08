88b4552a561ac90daf7947f1613f59d51390c1dc
Douglas <douglas.ferreira@varsis.com.br>
2023-08-08 18:38:13 -0300

    Singleton

---
+++ dgsGofSingleton/dgsGofSingleton/MyLog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dgsGofSingleton
+{
+    
+    public class MyLog
+    {
+        static private MyLog instancia = new MyLog();
+        private MyLog()
+        {                
+        }
+        
+        public void GravarLog(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static MyLog GetInstance()
+        {
+            return instancia;
+        }
+    }
+}
---
+++ dgsGofSingleton/dgsGofSingleton/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using dgsGofSingleton;
+
+ 
+MyLog log = MyLog.GetInstance();
+log.GravarLog("Implementando padrão Singleton do GOF - Gang of four");
+log.GravarLog("O singleton garante que a classe tera apenas uma unica instancia");
+log.GravarLog("O construtor precisa ser privado");
+log.GravarLog("Este é o padrao EAGER, tambem pode usar o LAZY que cria a instancia na primeira vez que for usada");
