6a0a8fe854c36bd0e22e1ce032de2ce03a91d9f1
Douglas Ferreira <54293352+douglasfsouza@users.noreply.github.com>
2024-06-29 14:30:28 -0300

    HttpClient to get a page

---
+++ dgHttpClient/dgHttpClient/Program.cs
@@ -0,0 +1,23 @@
+﻿string url = "https://pje.jfce.jus.br/pje/ConsultaPublica/listView.seam";
+
+HttpClient httpClient = new HttpClient
+{
+    BaseAddress = new Uri(url)
+};
+
+//ou
+
+HttpClient httpClient2 = new HttpClient();
+
+GetAsync(httpClient2,url).Wait();    
+static async Task GetAsync(HttpClient httpClient, string url)
+{
+    using HttpResponseMessage response = await httpClient.GetAsync(url);
+
+    response.EnsureSuccessStatusCode();
+    
+    var jsonResponse = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(jsonResponse);
+
+}
