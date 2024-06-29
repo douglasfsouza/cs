4c43c26ac563f985a89fa7d5c8795ecd81097148
Douglas Ferreira <54293352+douglasfsouza@users.noreply.github.com>
2024-06-29 11:15:17 -0300

    WebRequest

---
+++ dgWebRequest/dgWebRequest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+ 
+    WebRequest w;
+    string url = "https://pje.jfce.jus.br/pje/ConsultaPublica/listView.seam";
+
+    try
+    {
+        w = WebRequest.Create(url);
+
+    //se precisar usar proxy:
+    //WebProxy myProxy = new WebProxy("myproxy", 80);
+    //myProxy.BypassProxyOnLocal = true;
+
+    //w.Proxy = myProxy;
+
+    //ou pegar o proxy já definido no browser:
+   // w.Proxy = WebProxy.GetDefaultProxy();
+
+    Stream objStream;
+        objStream = w.GetResponse().GetResponseStream();
+
+        StreamReader objReader = new StreamReader(objStream);
+
+        string sLine = "";
+        int i = 0;
+
+        while (sLine != null)
+        {
+            i++;
+            sLine = objReader.ReadLine();
+            if (sLine != null)
+                Console.WriteLine("{0}:{1}", i, sLine);
+        }
+        Console.ReadLine();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+ 
