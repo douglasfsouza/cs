103149a18ada5a4478a44c4d5fad3102f3c4f8f4
Douglas Ferreira <54293352+douglasfsouza@users.noreply.github.com>
2026-03-12 12:24:56 -0300

    Ler o log RMSCTRL, somar updates e criar um arquivo com o resultado

---
+++ LerRMSLog/LerRMSLog/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+bool updateOn = false;
+double valor = 0;
+string log = "C:\\log\\jad.txt";
+StreamReader reader;
+StreamWriter writer;
+
+reader = File.OpenText(log);
+writer = File.CreateText("C:\\log\\updates.txt");
+
+while (!reader.EndOfStream)
+{
+    string line = reader.ReadLine();
+    if (updateOn)
+    {
+        Console.WriteLine(line);
+        valor += Convert.ToDouble(line.Split('=')[1].Replace(".",","));
+        updateOn = false;
+        writer.WriteLine(line);
+    }
+    if (line.Contains("UPDATE AA1RTITU"))
+    {
+        updateOn = true;                
+    }    
+}
+writer.Close();
+reader.Close();
+Console.WriteLine($"Total: {valor}");
+
